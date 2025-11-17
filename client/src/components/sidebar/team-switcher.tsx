f00b8f3d83388d2813fce5b4429deea6587db9f3
Thanh Nguyen <lethanh300504@gmail.com>
2025-11-17 23:34:35 +1100

    adjust styling

--- client/src/components/sidebar/team-switcher.tsx
+++ client/src/components/sidebar/team-switcher.tsx
@@ -1,37 +1,27 @@
 "use client";
 
 import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
   SidebarTrigger,
   useSidebar,
 } from "@/components/ui/sidebar";
 
 export function TeamSwitcher() {
-  const { isMobile } = useSidebar()
+  const { open } = useSidebar();
 
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <div className="flex items-center">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground flex-1"
-              >
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">chat app</span>
-                </div>
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-          </DropdownMenu>
-          <SidebarTrigger className="h-8 w-8" />
+        <div className="flex items-center justify-between group-data-[collapsible=icon]:justify-center">
+          <div className="flex h-12 flex-1 items-center gap-2 px-2 group-data-[collapsible=icon]:hidden">
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-bold text-2xl">
+                Lynx AI
+              </span>
+            </div>
+          </div>
+          <SidebarTrigger className="h-8 w-8 group-data-[collapsible=icon]:m-0" />
         </div>
       </SidebarMenuItem>
     </SidebarMenu>
